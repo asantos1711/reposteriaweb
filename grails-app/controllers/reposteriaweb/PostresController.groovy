3c4548c431ea0a59e564a6c4dec12e8bf0c9e8f9
abraham07 <asantos@sunset.com.mx>
2021-05-16 23:45:22 -0500

    cambios para postres

--- grails-app/controllers/reposteriaweb/PostresController.groovy
+++ grails-app/controllers/reposteriaweb/PostresController.groovy
@@ -1,14 +1,26 @@
 package reposteriaweb
 
 import productos.Productos
+import productos.Fotosproductos
 
 class PostresController {
 
     def index() { 
         
-        //Productos 
+        Map<Integer,List<Fotosproductos>> listaFotos = [:]
+        List<Productos> listaProductos = Productos.list()
         
-       render(view:"main")
+        listaProductos.each{it->
+            Productos p = it
+            listaFotos[p.id]=Fotosproductos.findAll("from Fotosproductos as b where b.producto = ${p.id}")
+        };
+        
+        listaFotos.each{k,v->
+            List<Fotosproductos> it =v
+            println it.first().url
+        };
+        
+       render(view:"main",model:[listaProductos:listaProductos, listaFotos:listaFotos])
     }
     
     
